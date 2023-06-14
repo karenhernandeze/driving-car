03458fa6ef343db9419443b9774e10c674a63a42
karenhernandeze <karenaliciahernandez@hotmail.com>
2023-06-14 04:31:12 -0600

    finished controls

--- src/main.js
+++ src/main.js
@@ -1,38 +1,21 @@
 import * as THREE from 'three';
-import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 
-let camera, scene, renderer, controls, groupCar;
-let speed = 0.5;
+let camera, scene, renderer, groupCar, light;
+
+let maxSpeed = 3;
+var speed = 0;
+var angle = 0;
+var steering = 0;
+
 
 init();
 animate();
 
-document.body.appendChild(renderer.domElement);
-
+document.addEventListener('keydown', handleKeyDown);
 
 function init() {
-    renderer = new THREE.WebGLRenderer({ antialias: true });
-    renderer.setPixelRatio(window.devicePixelRatio);
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    // renderer.setClearColor(0x242426);
-    // renderer.toneMapping = THREE.LinearToneMapping;
-
-    // renderer.shadowMap.enabled = true;
-    // renderer.shadowMap.type = THREE.PCFSoftShadowMap;
-
-    // --------------------------------------- ADD LATER -----------------------------------
-    // window.addEventListener( 'resize', function () {
-    //     camera.aspect = window.innerWidth / window.innerHeight;
-    //     camera.updateProjectionMatrix();
-    //     renderer.setSize( window.innerWidth, window.innerHeight );
-    //   }, false );
-
-    document.body.appendChild(renderer.domElement);
-
     scene = new THREE.Scene();
-    // scene.background = new THREE.Color(0xa0a0a0);
-    // scene.fog = new THREE.Fog(0x242426, 20, 600);
-
+    scene.fog = new THREE.Fog(0x242426, 20, 600);
     camera = new THREE.PerspectiveCamera(
         75,
         window.innerWidth / window.innerHeight,
@@ -40,37 +23,24 @@ function init() {
         600
     );
     camera.position.z = 90;
-    // camera.rotation.set(-90 * rad, -45 * rad, -20 * rad);
-    // camera.position.set(0, 60, 90);
-    scene.add(camera);
+
+    renderer = new THREE.WebGLRenderer({ antialias: true });
+    renderer.setPixelRatio(window.devicePixelRatio);
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.setClearColor(0x242426);
+    renderer.toneMapping = THREE.LinearToneMapping;
+
+    renderer.shadowMap.enabled = true;
+    renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+    document.body.appendChild(renderer.domElement);
 
     // --------------------------------------- LIGHTING -----------------------------------
     let hemiLight = new THREE.HemisphereLight(0xEBF7FD, 0xEBF7FD, 0.2);
-    //hemiLight.color.setRGB(0.75,0.8,0.95);
     hemiLight.position.set(0, 20, 20);
     scene.add(hemiLight);
 
-    // --------------------------------------- REMOVE -----------------------------------
-    controls = new OrbitControls(camera, renderer.domElement);
-    controls.enableDamping = true;
-    controls.minDistance = 0.5;
-    controls.maxDistance = 1000;
-
-    var light = new THREE.PointLight(0xFFFFFF, 1, 0);
-    light.position.z = 25;
-    light.position.x = 1;
-    light.castShadow = true;
-    // light.shadow.mapSize.width = 512;
-    // light.shadow.mapSize.height = 512;
-    // light.shadow.camera.near = 0.1;
-    light.shadow.camera.far = 50;
-    light.shadow.bias = 0.1;
-    // light.shadow.radius = 5;
-    light.power = 4;
-    scene.add(light);
-
     // --------------------------------------- CAR GEOMETRY -----------------------------------
-    const texture = new THREE.TextureLoader().load('./src/textures/car.jpeg');
+    const texture = new THREE.TextureLoader().load('./src/textures/mercedes.jpeg');
     texture.colorSpace = THREE.SRGBColorSpace;
 
     let carGeometry = new THREE.BoxGeometry(20, 10, 3);
@@ -84,7 +54,6 @@ function init() {
 
     let wheelGeometry = new THREE.CylinderGeometry(3, 3, 1, 6);
     let wheelMaterial = new THREE.MeshBasicMaterial({ color: 0x000000 });
-    let wheelMaterial2 = new THREE.MeshBasicMaterial({ color: 0x000000 });
 
     let carBody = new THREE.Mesh(carGeometry, carMaterial);
     carBody.castShadow = true;
@@ -104,7 +73,7 @@ function init() {
     wheel2.position.y = 6
     wheel2.position.x = -6
 
-    let wheel3 = new THREE.Mesh(wheelGeometry, wheelMaterial2);
+    let wheel3 = new THREE.Mesh(wheelGeometry, wheelMaterial);
     wheel3.position.y = -6
     wheel3.position.x = 6
 
@@ -135,11 +104,19 @@ function init() {
     light2.shadow.camera.near = 1;
     light2.target.position.y = 0.5;
     light2.target.position.x = 35;
-    // light2.shadow.mapSize.width = 512;
-    // light2.shadow.mapSize.height = 512;
-    // light2.shadow.camera.far = 400;
-    // light2.shadow.camera.fov = 40;
 
+    // --------------------------------------- LIGHT SURROUNDING CAR -----------------------------------
+    var light = new THREE.PointLight(0xFFFFFF, 1, 0);
+    light.position.z = 25;
+    light.position.x = 5;
+    light.castShadow = true;
+    light.shadow.mapSize.width = 512;
+    light.shadow.mapSize.height = 512;
+    light.shadow.camera.near = 0.1;
+    light.shadow.camera.far = 50;
+    light.shadow.bias = 0.1;
+    light.shadow.radius = 5;
+    light.power = 4; 
 
     // --------------------------------------- GROUP THE CAR -----------------------------------
     groupCar = new THREE.Group();
@@ -153,11 +130,8 @@ function init() {
     groupCar.add(light2);
     groupCar.add(light1.target);
     groupCar.add(light2.target);
+    groupCar.add(light)
     scene.add(groupCar)
-
-
-    // groupCar.rotation.y += THREE.MathUtils.degToRad(90); // Rotate by 1 degree
-
 
     // --------------------------------------- GROUND -----------------------------------
     function snowyGround() {
@@ -167,45 +141,104 @@ function init() {
         let segmentsY = 45;
 
         let geometry = new THREE.PlaneGeometry(width, height, segmentsX, segmentsY);
-
         let positionAttribute = geometry.attributes.position;
-
-
-        if (geometry.attributes.position.array.length === 0) {
-            console.error('Error: The geometry does not have any vertices.');
-            return null;
-        }
-
         positionAttribute.needsUpdate = true;
 
-        const textureMap = new THREE.TextureLoader().load('./src/textures/sand2.jpeg');
+        for (let j = 0; j <= segmentsY; j++) {
+            for (let i = 0; i <= segmentsX; i++) {
+                let vertexIndex = j * (segmentsX + 1) + i;
+                positionAttribute.array[vertexIndex * 3] += (Math.cos(vertexIndex * vertexIndex) + 1 / 2) * 2;
+                positionAttribute.array[vertexIndex * 3 + 1] += (Math.cos(vertexIndex) + 1 / 2) * 2;
+                positionAttribute.array[vertexIndex * 3 + 2] = (Math.sin(vertexIndex * vertexIndex * vertexIndex) + 1 / 2) * -4;
+            }
+        }
+
+        const textureMap = new THREE.TextureLoader().load('./src/textures/sand.jpeg');
         textureMap.colorSpace = THREE.SRGBColorSpace;
         // <------- FOR REPEATING THE TEXTURE, MAYBE NOT PUT IT -------> 
-        textureMap.repeat.set(20, 20); 
+        textureMap.repeat.set(20, 20);
         textureMap.wrapS = THREE.RepeatWrapping;
         textureMap.wrapT = THREE.RepeatWrapping;
 
         let material = new THREE.MeshPhongMaterial({
             map: textureMap,
-            bumpScale: 0.5,
-            shading: THREE.SmoothShading
+            color: 0xFFFFFF,
+            shininess: 80,
+            bumpScale: 0.25,
         });
 
         let plane = new THREE.Mesh(geometry, material);
         plane.receiveShadow = true;
         plane.position.z = -5;
         return plane;
+
     }
     scene.add(snowyGround());
-
-
 }
 
 
+function handleKeyDown(event) {
+    // Get the key code of the pressed key
+    const keyCode = event.keyCode;
+
+    var prev = {
+        x: groupCar.position.x,
+        y: groupCar.position.y,
+        rot: groupCar.rotation.z
+    }
+    var steerPower = 0.0006;
+
+    //Left and Right
+    if (keyCode == 37) {
+        // Left arrow key pressed
+        steering -= steerPower
+    } else if (keyCode === 39) {
+        // Right arrow key pressed
+        steering += steerPower
+    } else {
+        steering *= 0.92;
+    }
+
+    //Up and Dows
+    if (keyCode === 38) {
+        // Up arrow key pressed
+        speed = maxSpeed
+    } else if (keyCode === 40) {
+        // Down arrow key pressed
+        speed = -maxSpeed
+    } else {
+        speed *= 0.96;
+    }
+
+    speed *= 1 - Math.abs(steering / 2);
+    angle += steering * speed;
+
+
+    var xdir = speed * Math.cos(angle);
+    var ydir = speed * Math.sin(angle);
+
+    groupCar.position.x += xdir;
+    groupCar.position.y += -ydir;
+    groupCar.rotation.z = -angle;
+
+    groupCar.position.x = (groupCar.position.x > 990 || groupCar.position.x < -990 ? prev.x : groupCar.position.x);
+    groupCar.position.y = (groupCar.position.y > 990 || groupCar.position.y < -990 ? prev.y : groupCar.position.y);
+
+    camera.position.x += (groupCar.position.x - camera.position.x) * 0.1;
+    camera.position.y = (groupCar.position.y - 40 - (speed * 10));
+
+
+    camera.lookAt(
+        new THREE.Vector3(
+            groupCar.position.x,
+            groupCar.position.y,
+            0
+        )
+    );
+}
+
 function animate() {
     requestAnimationFrame(animate);
-    controls.update();
-    // groupCar.position.x += speed;
     render();
 }
 
